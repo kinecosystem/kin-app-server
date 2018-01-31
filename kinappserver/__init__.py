12b2152e8f719adad56de6f5f2abba98662985df
fuzzyami <ami.blonder@kik.com>
2018-01-31 15:53:20 +0200

    add flask-admin

--- kinappserver/__init__.py
+++ kinappserver/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +12,11 @@ from kinappserver import config
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_CONNSTR
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# TODO remove this on production
+admin = Admin(app, name='KipApp', template_mode='bootstrap3')
+from kinappserver.model import User
+admin.add_view(ModelView(User, db.session))
 
 import kinappserver.views
 import time
