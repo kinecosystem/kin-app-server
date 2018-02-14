e7c7c6bb528a5aac5c1cd8325c744a57192be839
fuzzyami <ami.blonder@kik.com>
2018-02-14 13:46:07 +0200

    move kin issuer address to config

--- kinappserver/stellar.py
+++ kinappserver/stellar.py
@@ -1,4 +1,4 @@
-from kinappserver import app
+from kinappserver import app, config
 
 def create_account(public_address, initial_xlm_amount):
     '''create an account for the given public address'''
@@ -7,7 +7,7 @@ def create_account(public_address, initial_xlm_amount):
 
 def send_kin(public_address, amount, memo=None):
     '''send kins to an address'''
-    print('address: %s' % public_address)
+    print('sending kin to address: %s' % public_address) #TODO REMOVE
     from stellar_base.asset import Asset
-    kin_asset = Asset('KIN', 'GCKG5WGBIJP74UDNRIRDFGENNIH5Y3KBI5IHREFAJKV4MQXLELT7EX6V')
+    kin_asset = Asset('KIN', config.STELLAR_KIN_ISSUER_ADDRESS)
     return app.kin_sdk.send_asset(public_address, kin_asset, amount, memo)
