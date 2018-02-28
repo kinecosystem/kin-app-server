84e4febc703ac07297ad5e6f3edee3378d1c5fca
fuzzyami <ami.blonder@kik.com>
2018-02-28 12:32:36 +0200

    switch user_id to header in user/tasks/

--- kinappserver/views.py
+++ kinappserver/views.py
@@ -199,7 +199,7 @@ def add_task_api():
 @app.route('/user/tasks', methods=['GET'])
 def get_next_task():
     '''returns the current task for the user with the given id'''
-    user_id = request.args.get('user-id', None)
+    user_id = extract_header(request)
     tasks = []
     for tid in get_task_ids_for_user(user_id):
         tasks.append(get_task_by_id(tid))
