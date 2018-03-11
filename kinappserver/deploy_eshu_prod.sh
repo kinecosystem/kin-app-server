1da0428bd75f7930308ad22d8d6ebddfad7a9b17
fuzzyami <ami.blonder@kik.com>
2018-03-11 19:55:57 +0200

    add helper shell scripts

---
+++ kinappserver/deploy_eshu_prod.sh
@@ -0,0 +1,1 @@
+ansible-playbook playbooks/kin-app-eshu-backend-prod.yml -i kin-app-eshu-backend-prod1,kin-app-eshu-backend-prod2 -e 'ansible_python_interpreter=/usr/bin/python3'
---
+++ kinappserver/deploy_server_stage.sh
@@ -0,0 +1,1 @@
+ansible-playbook playbooks/kin-app-server-stage.yml -i kin-app-server-stage, -e 'ansible_python_interpreter=/usr/bin/python3'
