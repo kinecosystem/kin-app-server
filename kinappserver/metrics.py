ab6e27256d809004bea0a9c2d0be8e9d69beac4d
fuzzyami <ami.blonder@kik.com>
2018-06-07 18:27:25 +0300

    add balance tracking

--- kinappserver/metrics.py
+++ kinappserver/metrics.py
@@ -22,4 +22,19 @@ def report_inventory():
         statsd.gauge('kinitapp.%s.%s' % (os.environ['ENV'], metric_name_total), inventory[offer_id]['total'])
 
 
+def report_bh_balance():
+    """tracks the current balance of our blackhawk account"""
+    balance = 0
+    response = requests.get(URL_PREFIX + '/blackhawk/account/balance')
+    try:
+        balance = json.loads(response.text)['balance']
+    except Exception as e:
+        print('cant collect balance')
+        pass
+
+        metric = 'bh-account-balance'
+        statsd.gauge('kinitapp.%s.%s' % (os.environ['ENV'], metric), balance)
+
+
 report_inventory()
+report_bh_balance()
