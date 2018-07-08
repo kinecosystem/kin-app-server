9dd8fa4a551130cabbabd50966828866deb2a4c5
fuzzyami <ami.blonder@kik.com>
2018-07-08 09:51:17 +0300

    tweak deploy scripts

--- kinappserver/deploy_server_stage.sh
+++ kinappserver/deploy_server_stage.sh
@@ -1,3 +1,4 @@
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # https://github.com/ansible/ansible/issues/32499
 ansible-playbook playbooks/kin-app-server-stage.yml -i kin-app-server-stage, -e 'ansible_python_interpreter=/usr/bin/python3'
 
 
