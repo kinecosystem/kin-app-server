1f35529c329adf5cb17d05ba05f6c23c316d93f8
fuzzyami <ami.blonder@kik.com>
2018-07-09 10:25:32 +0300

    hide sql user/password

--- kinappserver/deploy_server_stage2_bin.sh
+++ kinappserver/deploy_server_stage2_bin.sh
@@ -1,1 +1,3 @@
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # https://github.com/ansible/ansible/issues/32499
 ansible-playbook playbooks/kin-app-server-stage2-bin.yml -i kin-app-server-stage-2, -e 'ansible_python_interpreter=/usr/bin/python3'
+
--- kinappserver/deploy_server_stage_bin.sh
+++ kinappserver/deploy_server_stage_bin.sh
@@ -1,1 +1,3 @@
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # https://github.com/ansible/ansible/issues/32499
 ansible-playbook playbooks/kin-app-server-stage-bin.yml -i kin-app-server-stage, -e 'ansible_python_interpreter=/usr/bin/python3'
+
