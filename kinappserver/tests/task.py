87fc4c19a2f9b68b4c3e0d48965ec3fa5788cb63
fuzzyami <ami.blonder@kik.com>
2018-07-29 16:09:17 +0300

    add quiz task checks

--- kinappserver/tests/task.py
+++ kinappserver/tests/task.py
@@ -68,6 +68,47 @@ class Tester(unittest.TestCase):
                             content_type='application/json')
         self.assertEqual(resp.status_code, 200)
 
+
+        quiz_task = { 'id': 1,
+                  'title': 'do you know horses?',
+                  'desc': 'horses_4_dummies',
+                  'type': 'quiz',
+                  'price': 2000,
+                  'min_to_complete': 2,
+                  'skip_image_test': False,
+                  'start_date': '2013-05-11T21:23:58.970460+00:00',
+                  'tags': ['music',  'crypto', 'movies', 'kardashians', 'horses'],
+                  'provider':
+                    {'name': 'om-nom-nom-food', 'image_url': 'https://s3.amazonaws.com/kinapp-static/brand_img/gift_card.png'},
+                  'items': [
+                    {
+                     'id': '435',
+                     'text': 'what animal is this?',
+                     'image_url': 'https://s3.amazonaws.com/kinapp-static/brand_img/gift_card.png',
+                        'quiz_data': {
+                            'answer_id': '235',
+                            'explanation': 'Of course it is!',
+                            'reward': 5},
+                                'type': 'textimage',
+                         'results': [
+                                {'id': '235',
+                                 'text': 'a horse!',
+                                 'image_url': 'https://s3.amazonaws.com/kinapp-static/brand_img/gift_card.png'},
+                                    {'id': '2465436',
+                                 'text': 'a cat!',
+                                 'image_url': 'https://s3.amazonaws.com/kinapp-static/brand_img/gift_card.png'},
+                                 ],
+                    }]
+            }
+
+        resp = self.app.post('/task/add',
+                            data=json.dumps({
+                            'task': quiz_task}),
+                            headers={},
+                            content_type='application/json')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 200)
+
         print(models.list_all_task_data())
 
         # register a user
@@ -93,6 +134,9 @@ class Tester(unittest.TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertNotEqual(data['tasks'][0]['memo'], None)
         self.assertEqual(models.get_user_task_results(userid), [])
+
+
+
 
 
 
