06f424a38491856163e68778ba9d8fe0e0b091f3
fuzzyami <ami.blonder@kik.com>
2018-08-23 20:50:20 +0300

    limit the number of missing txs to 500

--- kinappserver/models/user.py
+++ kinappserver/models/user.py
@@ -735,54 +735,11 @@ def find_missing_txs():
             # get the active user id for the phone number
             missing_txs.append({'user_id': get_active_user_id_by_enc_phone(enc_number), 'task_id': item, 'reward': reward})
 
+        if len(missing_txs) > 500:
+            # stop here, otherwise it takes too long
+            break
+
     return missing_txs
-
-
-def find_missing_txs2():
-    users = User.query.all()
-    missing_txs = []
-    num_missing_txs_by_user = {}
-    for user in users:
-        missing_txs_count = 0
-        #print('checking user %s: ' % user.user_id)
-
-        user_app_data = get_user_app_data(user.user_id)
-        completed_tasks = user_app_data.completed_tasks
-
-        # get the task results
-        from .task import get_user_task_results
-        user_task_results_ids_dict = {result.task_id: result.update_at for result in get_user_task_results(user.user_id)}
-        #import json
-        #for task_id in json.loads(completed_tasks):
-        #    if task_id not in user_task_results_ids_dict.keys():
-        #        print('cant find task results for task id (%s) for userid (%s)' % (task_id, user.user_id))
-        #        pass
-        #    else:
-        #        #  print('found task results for task_id %s for user_id (%s)' % (task_id, user.user_id))
-        #        pass
-
-        from .transaction import list_user_transactions
-        user_txs = list_user_transactions(user.user_id)
-        tx_task_ids = []
-        for tx in user_txs:
-            if not tx.incoming_tx:
-                tx_task_ids.append(tx.tx_info['task_id'])
-
-        # see if there any task results for which we have no txs
-        for task_id in user_task_results_ids_dict.keys():
-            if task_id not in tx_task_ids:
-                from .task import get_reward_for_task
-                reward = get_reward_for_task(task_id)
-                #print('cant find tx for task_id %s for user_id %s' % (task_id, user.user_id))
-                missing_txs.append((user.user_id, task_id, reward, user_task_results_ids_dict[task_id]))
-                missing_txs_count = missing_txs_count + 1
-            else:
-                #  print('found tx for task id (%s) for user_id (%s)' % (task_id, user.user_id))
-                pass
-        if missing_txs_count > 0:
-            num_missing_txs_by_user[str(user.user_id)] = missing_txs_count
-    return missing_txs
-
 
 def nuke_user_data(phone_number, nuke_all = False):
     """nuke user's data by phone number. by default only nuke the active user"""
