31c094078990dc9d3a9a5173717297b17fc50d98
fuzzyami <ami.blonder@kik.com>
2018-08-26 10:07:03 +0300

    removed stage2

--- kinappserver/deploy_server_stage2.sh
+++
@@ -1,5 +0,0 @@
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # https://github.com/ansible/ansible/issues/32499
-ansible-playbook playbooks/kin-app-server-stage2.yml -i kin-app-server-stage-2, -e 'ansible_python_interpreter=/usr/bin/python3'
-
-
-
