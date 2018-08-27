644b827df37df08c395cb1602ff7c4983966cbd4
fuzzyami <ami.blonder@kik.com>
2018-08-27 14:35:29 +0300

    tweak blocked prefixes in prod

--- kinappserver/views.py
+++ kinappserver/views.py
@@ -203,10 +203,11 @@ def set_user_phone_number_endpoint():
             print('bad id-token: %s' % token)
             return jsonify(status='error', reason='bad_token'), status.HTTP_404_NOT_FOUND
 
-        for prefix in app.blocked_phone_prefixes:
-            if verified_number.find(prefix) == 0:
-                print('found blocked phone prefix (%s) in verified phone number (%s): aborting' % (prefix, verified_number))
-                abort(403)
+        #TODO uncomment this later today
+        #for prefix in app.blocked_phone_prefixes:
+        #    if verified_number.find(prefix) == 0:
+        #        print('found blocked phone prefix (%s) in verified phone number (%s): aborting' % (prefix, verified_number))
+        #        abort(403)
 
         phone = verified_number
     else: #DEBUG
