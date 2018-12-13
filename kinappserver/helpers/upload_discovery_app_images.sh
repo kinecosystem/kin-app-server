89be900227095969ee1ba2337509959ff886bb0a
Aryeh Katz <in@akatz.io>
2018-12-13 16:42:32 +0200

    upload discovery apps with bash

--- kinappserver/helpers/upload_discovery_app_images.sh
+++ kinappserver/helpers/upload_discovery_app_images.sh
@@ -1,26 +1,21 @@
 #! /bin/bash
 
 # this script uploads a file from your local machine to our s3 bucket.
-# specifically, the files are uploaded to the brand_img folder. 
-# once uploaded they become available through this url:
-# https://s3.amazonaws.com/kinapp-static/brand_img/<your file name>
-# to use this script, you need a few things:
-# open a terminal and then - 
-# install the AWSCLI with 'brew install awscli' (you only need to do this once)
-# then export the secret key which Ami sent you via slack, using this command:
-# export AWS_SECRET_ACCESS_KEY=<the secret access key you got from Ami>
-# finally, you can now use the script to upload files:
-# ./upload_to_s3.sh <your file>
-# for example,
-# ./upload_to_s3.sh ~/Downloads/cats_r_great.png
 
-aws --version
-filename=$(basename $1)
-aws s3 cp $1 s3://kinapp-static/discovery/kinit/android/mdpi/ --acl public-read
-aws s3 cp $1 s3://kinapp-static/discovery/kinit/android/hdpi/ --acl public-read
-aws s3 cp $1 s3://kinapp-static/discovery/kinit/android/xhdpi/ --acl public-read
-aws s3 cp $1 s3://kinapp-static/discovery/kinit/android/xxhdpi/ --acl public-read
-aws s3 cp $1 s3://kinapp-static/discovery/kinit/android/xxxhdpi/ --acl public-read
-aws s3 cp $1 s3://kinapp-static/discovery/kinit/ios/ --acl public-read
-aws s3 cp $1 s3://kinapp-static/discovery/kinit/ios/$filename@2x --acl public-read
-aws s3 cp $1 s3://kinapp-static/discovery/kinit/ios/$filename@3x --acl public-read
+for file in $(find $1 -name '*.png' -or -name '*.jpg' -maxdepth 1)
+do
+filename=$(basename -- "$file")
+extension="${filename##*.}"
+filename="${filename%.*}"
+aws s3 cp $file s3://kinapp-static/discovery/{}/android/mdpi/ --acl public-read
+aws s3 cp $file s3://kinapp-static/discovery/{}/android/hdpi/ --acl public-read
+aws s3 cp $file s3://kinapp-static/discovery/{}/android/xhdpi/ --acl public-read
+aws s3 cp $file s3://kinapp-static/discovery/{}/android/xxhdpi/ --acl public-read
+aws s3 cp $file s3://kinapp-static/discovery/{}/android/xxxhdpi/ --acl public-read
+aws s3 cp $file s3://kinapp-static/discovery/{}/ios/ --acl public-read
+aws s3 cp $file s3://kinapp-static/discovery/{}/ios/$filename@2x.$extension --acl public-read
+aws s3 cp $file s3://kinapp-static/discovery/{}/ios/$filename@3x.$extension --acl public-read
+echo $file
+done
+
+ aws s3 cp $1/logo/ s3://kinapp-static/discovery/{}/logo --recursive --acl public-read
