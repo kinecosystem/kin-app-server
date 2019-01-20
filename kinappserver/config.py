72c2163a4fbd86fb24ac703bd5bcdf22a2053ac1
Aryeh Katz <in@akatz.io>
2019-01-20 18:39:34 +0200

    add zendesk api token

--- kinappserver/config.py
+++ kinappserver/config.py
@@ -83,3 +83,4 @@ OFFER_RATE_LIMIT_MIN_IOS_VERSION = '1.2.1'
 OFFER_RATE_LIMIT_MIN_ANDROID_VERSION = '1.4.1'
 
 USER_LOCKED_OFFERS_REDIS_KEY = 'REDIS_USER_BLOCKED_OFFERS_LIST_%s'
+ZENDESK_API_TOKEN = "this gets overwritten by the tester code. it acutally uses a temp postgress db on the local disc"
