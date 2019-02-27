bd0d519a69bcfd15b27932b6c4118d1925aff988
Aryeh Katz <in@akatz.io>
2019-02-27 15:34:52 +0200

    still crashing

--- kinappserver/models/task2.py
+++ kinappserver/models/task2.py
@@ -304,12 +304,14 @@ def get_next_tasks_for_user(user_id, source_ip=None, cat_ids=[], send_push=True)
         # log.info('tasks_per_category: num of tasks for user %s for cat_id %s: %s' % (user_id, cat_id, len(tasks_per_category[cat_id])))
 
     # loop all categories and check if at least one task available
+    log.info("checking if all tasks were filtered\n%s" %
+             tasks_per_category)
     if all(t is "filtered" for t in tasks_per_category.values()):
         return None
 
     # there is at least 1 task, the client expects an array
     for cat_index in tasks_per_category.keys():
-        if tasks_per_category[cat_index] is None:
+        if tasks_per_category[cat_index] is None or tasks_per_category[cat_index] == "filtered":
             tasks_per_category[cat_index] = []
         else:
             tasks_per_category[cat_index] = [tasks_per_category[cat_index]]
