85002d83a8cf13f30a042f09508db46cc3f49796
Berry Ventura Lev <berry@kik.com>
2019-05-05 12:36:08 +0300

    removing redundant deployment scripts

--- kinappserver/restart_server_prod.sh
+++ kinappserver/restart_server_prod.sh
@@ -1,4 +1,4 @@
 export AWS_REGION='us-east-1'
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # https://github.com/ansible/ansible/issues/32499
 
-ansible-playbook playbooks/kin-app-server-prod-restart-only.yml -i kin-app-server-prod-3 --extra-vars "kinit_prod_tg_arn='arn:aws:elasticloadbalancing:us-east-1:935522987944:targetgroup/kinitapp-prod/4311f4679bb3c46b'"
+ansible-playbook playbooks/kin-app-server-prod-restart-only.yml -i kin-app-server-prod-3, --extra-vars "kinit_prod_tg_arn='arn:aws:elasticloadbalancing:us-east-1:935522987944:targetgroup/kin-app-server-prod-3/749e2708dfef1cf3'"
