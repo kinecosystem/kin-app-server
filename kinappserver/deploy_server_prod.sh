b26673b2f2beaa7e0b4fc7eb6f8935f64eb10b5f
Berry Ventura Lev <berry@kik.com>
2019-05-12 15:12:06 +0300

    change deployment server names

--- kinappserver/deploy_server_prod.sh
+++ kinappserver/deploy_server_prod.sh
@@ -1,3 +1,3 @@
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # https://github.com/ansible/ansible/issues/32499
-ansible-playbook playbooks/kin-app-server-prod.yml -i kin-app-server-prod-3, -e 'ansible_python_interpreter=/usr/bin/python3'
+ansible-playbook playbooks/kin-app-server-prod.yml -i kinit-server-prod-kin3-2, kinit-server-prod-kin3-1, -e 'ansible_python_interpreter=/usr/bin/python3'
 
