6559509e95f1d25e0c943c681aef148d3e28c8ae
Berry Ventura Lev <berry@kik.com>
2019-06-18 14:31:54 +0300

    fix restart script

--- kinappserver/restart_server_prod.sh
+++ kinappserver/restart_server_prod.sh
@@ -1,4 +1,4 @@
 export AWS_REGION='us-east-1'
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # https://github.com/ansible/ansible/issues/32499
 
-ansible-playbook playbooks/kin-app-server-prod-restart-only.yml -i kin-app-server-prod-3, --extra-vars "kinit_prod_tg_arn='arn:aws:elasticloadbalancing:us-east-1:935522987944:targetgroup/kin-app-server-prod-3/749e2708dfef1cf3'"
+ansible-playbook playbooks/kin-app-server-prod-restart-only.yml -i kinit-server-prod-kin3-2,kinit-server-prod-kin3-1, --extra-vars "kinit_prod_tg_arn='arn:aws:elasticloadbalancing:us-east-1:935522987944:targetgroup/ami-payment/b58102c0b5108c1e'"
